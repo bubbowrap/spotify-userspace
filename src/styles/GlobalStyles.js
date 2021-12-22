9760d5088765566bfa6884fcbe1c770ffb6b1d57
Jeremy Samuel <bubbowrap@gmail.com>
2021-12-22 16:39:59 -0500

    folder setup

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,8 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
+import theme from './theme';
+
+const { colors } = theme;
 
 const GlobalStyles = createGlobalStyle`
     html {
         box-sizing: border-box;
+        background: ${colors.black}
     }
 
     *,
